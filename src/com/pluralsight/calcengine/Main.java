475d23c6495e38b552767277ac5cab09ec015f91
Shiblon <spahulu@byu.edu>
2019-01-29 19:24:28 -0600

    brach created to add my classes

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -3,31 +3,28 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-        double [] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-        double [] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        char [] opCodes = {'d', 'a', 's', 'm'};
-        double [] results = new double [opCodes.length];
-        for (int i=0; i<opCodes.length; i++) {
-            switch(opCodes[i]) {
-                case 'a':
-                    results[i] = leftVals[i] + rightVals[i];
-                    break;
-                case's':
-                    results[i] = leftVals[i] - rightVals[i];
-                    break;
-                case 'd':
-                    results[i] = rightVals[i] == 0.0d ? 0.0d : leftVals[i] / rightVals[i];
-                    break;
-                case 'm':
-                    results[i] = leftVals[i] * rightVals[i];
-                    break;
-                default:
-                    System.out.println("Error - invalid opCode");
-                    results[i] = 0.0d;
-                    break;
-            }
+        //double [] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        //double [] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        //char [] opCodes = {'d', 'a', 's', 'm'};
+        //double [] results = new double [opCodes.length];
+        MathEquation [ ] equations = new MathEquation[4];
+        equations[0] = create(100.0d, 50.0d,'d');
+        equations[1] = create(25.0d, 92.0d,'a');
+        equations[2] = create(225.0d, 17.0d,'s');
+        equations[0] = create(11.0d, 3.0d,'m');
+
+        for(MathEquation equation : equations){
+            equation.execute();
+            System.out.println(equation.result);
         }
-        for(double theResults : results)
-        System.out.println(theResults);
+    }
+    public static MathEquation create(double leftVal, double rightVal, char opCode)
+    {
+        MathEquation equation = new MathEquation();
+        equation.leftVal=leftVal;
+        equation.rightVal=rightVal;
+        equation.opCode=opCode;
+
+        return equation;
     }
 }
